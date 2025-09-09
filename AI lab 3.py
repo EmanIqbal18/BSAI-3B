16a8379a17d0de589424efd242cbd1587592501f
EmanIqbal18 <iqbaleman17@gmail.com>
2025-09-09 12:19:35 +0500

    Add files via upload

---
+++ AI lab 3.py
@@ -0,0 +1,40 @@
+class Model_Reflex_Agent:
+    def __init__(self, set_temp):
+        self.goal_temp = set_temp
+        self.prev_action = None
+
+    def read_temp(self, room_temp):
+        self.room_temp = room_temp
+
+    def performance(self):
+        if self.room_temp > self.goal_temp:
+            action = "Turn ON the AC"
+        else:
+            action = "Turn OFF the AC"
+
+        if action == self.prev_action:
+            action = "NO change (the model will hold the same state)"
+        else:
+            self.prev_action = action
+        return action
+    
+    def actuator(self):
+        choice = self.performance()
+        print(self.room_temp, "=>", choice)
+
+
+rooms = {
+    "Living Room": 20,
+    "Kitchen": 34,
+    "Drawing Room": 22,
+    "Study Room": 20,
+    "Guest Room": 26
+}
+
+agent = Model_Reflex_Agent(21)
+
+for room, temp in rooms.items():
+    print(room, end=" :\t")
+    agent.read_temp(temp)
+    agent.actuator()
+
